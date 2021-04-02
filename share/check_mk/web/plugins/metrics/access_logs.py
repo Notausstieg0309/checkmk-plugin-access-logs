964d78bc0f062617ec919a399671995cd48ccf6d
Notausstieg0309 <34344724+Notausstieg0309@users.noreply.github.com>
2021-04-02 14:56:38 +0200

    renaming perfdata
    With CheckMK 2.0.0 it is not possible to use perfdata names starting with a number anymore as those names were falsly identified as float number by CheckMK. So all perfdata has to be renamed.
    
    This means, that any historical data cannot be used anymore.
    
    Sorry...

--- share/check_mk/web/plugins/metrics/access_logs.py
+++ share/check_mk/web/plugins/metrics/access_logs.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python
 # -*- encoding: utf-8; py-indent-offset: 4 -*-
 
-# Metrics beginning with numbers and ending with letters needs to be renamed (???)
-check_metrics["check_mk-access_logs"] = {
-        "1XX"   : { "name" : "s1XX" },
-        "2XX"   : { "name" : "s2XX" },
-        "3XX"   : { "name" : "s3XX" },
-        "4XX"   : { "name" : "s4XX" },
-        "5XX"   : { "name" : "s5XX" },
-        }
+from cmk.gui.plugins.metrics import (
+   metric_info,
+   graph_info,
+)
+
 
 
 # Status 1xx
 
-metric_info["101"] = {
+metric_info["count_101"] = {
     "title" : _("101 - Switching Protocols"),
     "unit"  : "count",
     "color" : "41/a",
 }
 
-metric_info["s1XX"] = {
+metric_info["count_1XX"] = {
     "title" : _("1XX - (Information)"),
     "unit"  : "count",
     "color" : "41/b",
@@ -28,13 +25,13 @@ metric_info["s1XX"] = {
 
 # Status 2xx
 
-metric_info["200"] = {
+metric_info["count_200"] = {
     "title" : _("200 - OK"),
     "unit"  : "count",
     "color" : "26/a",
 }
 
-metric_info["s2XX"] = {
+metric_info["count_2XX"] = {
     "title" : _("2XX - Successful"),
     "unit"  : "count",
     "color" : "26/b",
@@ -43,8 +40,8 @@ metric_info["s2XX"] = {
 
 # Status 3xx
 
-metric_info["s3XX"] = {
     "title" : _("3XX - Successful"),
+metric_info["count_3XX"] = {
     "unit"  : "count",
     "color" : "24/a",
 }
@@ -52,31 +49,31 @@ metric_info["s3XX"] = {
 
 # Status 4xx
 
-metric_info["400"] = {
+metric_info["count_400"] = {
     "title" : _("400 - Bad Request"),
     "unit"  : "count",
     "color" : "21/a",
 }
 
-metric_info["401"] = {
+metric_info["count_401"] = {
     "title" : _("401 - Unauthorized"),
     "unit"  : "count",
     "color" : "16/a",
 }
 
-metric_info["403"] = {
+metric_info["count_403"] = {
     "title" : _("403 - Forbidden"),
     "unit"  : "count",
     "color" : "15/a",
 }
 
-metric_info["404"] = {
+metric_info["count_404"] = {
     "title" : _("404 - Not Found"),
     "unit"  : "count",
     "color" : "14/a",
 }
 
-metric_info["s4XX"] = {
+metric_info["count_4XX"] = {
     "title" : _("4XX - Client Error"),
     "unit"  : "count",
     "color" : "13/a",
@@ -85,31 +82,31 @@ metric_info["s4XX"] = {
 
 # Status 5xx
 
-metric_info["500"] = {
+metric_info["count_500"] = {
     "title" : _("500 - Internal Server Error"),
     "unit"  : "count",
     "color" : "12/a",
 }
 
-metric_info["502"] = {
+metric_info["count_502"] = {
     "title" : _("502 - Bad Gateway"),
     "unit"  : "count",
     "color" : "11/a",
 }
 
-metric_info["503"] = {
+metric_info["count_503"] = {
     "title" : _("503 - Service Unavailable"),
     "unit"  : "count",
     "color" : "46/a",
 }
 
-metric_info["504"] = {
+metric_info["count_504"] = {
     "title" : _("504 - Gateway Timeout"),
     "unit"  : "count",
     "color" : "45/a",
 }
 
-metric_info["s5XX"] = {
+metric_info["count_5XX"] = {
     "title" : _("5XX - Server Error"),
     "unit"  : "count",
     "color" : "12/b",
@@ -118,13 +115,13 @@ metric_info["s5XX"] = {
 
 # everything else
 
-metric_info["XXX"] = {
+metric_info["count_XXX"] = {
     "title" : _("XXX - [other status codes]"),
     "unit"  : "count",
     "color" : "51/a",
 }
 
-metric_info["no_status"] = {
+metric_info["count_no_status"] = {
     "title" : _("No status code"),
     "unit"  : "count",
     "color" : "52/a",
@@ -139,23 +136,24 @@ metric_info["failure_rate"] = {
 graph_info.append({
    "title"         : ("HTTP Response Status Codes"),
    "metrics"       : [
-                        ( "no_status", "area" ),
-                        ( "XXX", "stack" ),
-                        ( "s5XX", "stack" ),
-                        ( "504", "stack" ),
-                        ( "503", "stack" ),
-                        ( "502", "stack" ),
-                        ( "500", "stack" ),
-                        ( "s4XX", "stack" ),
-                        ( "404",  "stack" ),
-                        ( "403",  "stack" ),
-                        ( "401",  "stack" ),
-                        ( "400",  "stack" ),
-                        ( "s3XX", "stack" ),
-                        ( "s2XX", "stack" ),
-                        ( "200",  "stack" ),
-                        ( "s1XX", "stack" ),
-                        ( "101", "stack")
+                        ( "count_no_status", "area" ),
+
+                        ( "count_XXX", "stack" ),
+                        ( "count_5XX", "stack" ),
+                        ( "count_504", "stack" ),
+                        ( "count_503", "stack" ),
+                        ( "count_502", "stack" ),
+                        ( "count_500", "stack" ),
+                        ( "count_4XX", "stack" ),
+                        ( "count_404",  "stack" ),
+                        ( "count_403",  "stack" ),
+                        ( "count_401",  "stack" ),
+                        ( "count_400",  "stack" ),
+                        ( "count_3XX", "stack" ),
+                        ( "count_2XX", "stack" ),
+                        ( "count_200",  "stack" ),
+                        ( "count_1XX", "stack" ),
+                        ( "count_101", "stack")
                      ],
     "omit_zero_metrics" : True,
 })
